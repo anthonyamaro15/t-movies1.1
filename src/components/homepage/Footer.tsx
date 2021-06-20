9f57c763cb3b053b4a96245658d89b59722798a3
Anthony Amaro <aamaro@bradmins-MacBook-Pro.local>
2021-06-20 08:57:12 -0700

    feat: add footer

---
+++ src/components/homepage/Footer.tsx
@@ -0,0 +1,78 @@
+const Footer = () => {
+   return (
+      <footer className="Footer">
+         <p>Questions? Call 1-844-505-2993</p>
+         <nav>
+            <ul>
+               <li>
+                  <a href="www.google.com">FAQ</a>
+               </li>
+               <li>
+                  <a href="www.google.com">Investor Relations</a>
+               </li>
+               <li>
+                  <a href="www.google.com">Ways to Watch</a>
+               </li>
+               <li>
+                  <a href="www.google.com">Corporate Information</a>
+               </li>
+               <li>
+                  <a href="www.google.com">Netflix Originals</a>
+               </li>
+            </ul>
+            <ul>
+               <li>
+                  <a href="www.google.com">Help Center</a>
+               </li>
+               <li>
+                  <a href="www.google.com">Jobs</a>
+               </li>
+               <li>
+                  <a href="www.google.com">Terms of Use</a>
+               </li>
+               <li>
+                  <a href="www.google.com">Contact Us</a>
+               </li>
+            </ul>
+            <ul>
+               <li>
+                  <a href="www.google.com">Account</a>
+               </li>
+               <li>
+                  <a href="www.google.com">Redeem Gift Cards</a>
+               </li>
+               <li>
+                  <a href="www.google.com">Privacy</a>
+               </li>
+               <li>
+                  <a href="www.google.com">Speed Test</a>
+               </li>
+            </ul>
+            <ul>
+               <li>
+                  <a href="www.google.com">Media Center</a>
+               </li>
+               <li>
+                  <a href="www.google.com">Buy Gift Cards</a>
+               </li>
+               <li>
+                  <a href="www.google.com">Cookie Preference</a>
+               </li>
+               <li>
+                  <a href="www.google.com">Legal Notices</a>
+               </li>
+            </ul>
+         </nav>
+         <div className="language">
+            <label htmlFor="language">
+               <select name="language" id="language">
+                  <option value="english">English</option>
+                  <option value="spanish">Spanish</option>
+               </select>
+            </label>
+         </div>
+      </footer>
+   );
+};
+
+export default Footer;
